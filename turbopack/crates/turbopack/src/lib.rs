3ea35f90ec282579a184f0cf9ea021fa19cbbd83
Luke Sandberg <lukeisandberg@gmail.com>
2025-12-01 13:36:15 -0800

    fix a name

--- turbopack/crates/turbopack/src/lib.rs
+++ turbopack/crates/turbopack/src/lib.rs
@@ -296,7 +296,6 @@ async fn apply_module_type(
     if tree_shaking_mode.is_some() && is_evaluation {
         // If we are tree shaking, skip the evaluation part if the module is marked as
         // side effect free.
-
         if *module.side_effects().await? == ModuleSideEffects::SideEffectFree {
             return Ok(ProcessResult::Ignore.cell());
         }
